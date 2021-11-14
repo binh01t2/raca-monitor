0d873aade665463df0634783585630136ebbc3db
Binh Nguyen <tinyswish@gmail.com>
2021-11-14 16:54:01 +0900

    First commit

---
+++ raca-monitor.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const chalk = require('chalk');
+
+request('https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=egg',
+    { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(err); }
+        console.log(chalk.green('Lowest egg price:') + chalk.red(body['list'][0]['fixed_price']));
+    });
+request('https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=yellow',
+    { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(err); }
+        console.log(chalk.green('Lowest yellow diamond price:') + chalk.red(body['list'][0]['fixed_price']));
+    });
+request('https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=metamon&saleType&category=13&tokenType',
+    { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(err); }
+        console.log(chalk.green('Lowest metamon price:') + chalk.red(body['list'][0]['fixed_price']));
+});
