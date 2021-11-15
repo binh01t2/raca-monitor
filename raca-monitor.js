f74fb8f62b274ae819e1df87b8cf47c8e88874b0
Binh Nguyen <tinyswish@gmail.com>
2021-11-15 11:32:42 +0900

    add KISS land and purple diamond price

--- raca-monitor.js
+++ raca-monitor.js
@@ -28,5 +28,17 @@ function showinfo()
                 return console.log(err); }
             console.log(chalk.magenta('Lowest potion price:') + chalk.red(body['list'][0]['fixed_price']));
     });
+    request('https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=&saleType&category=20&tokenType',
+        { json: true }, (err, res, body) => {
+            if (err) {
+                return console.log(err); }
+            console.log(chalk.cyan('Lowest KISS land price:') + chalk.red(body['list'][0]['fixed_price']));
+    });
+    request('https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=fixed_price&order=asc&name=purple',
+        { json: true }, (err, res, body) => {
+            if (err) {
+                return console.log(err); }
+            console.log(chalk.magentaBright('Lowest purple diamond price:') + chalk.red(body['list'][0]['fixed_price']));
+    });
 }
 setInterval(showinfo,10000);
